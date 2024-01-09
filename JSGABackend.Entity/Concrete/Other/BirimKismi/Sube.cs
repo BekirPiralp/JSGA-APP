c334cbc792245f5bacab912b7d88f6835addc2de
Bekir PİRALP <bpir33@gmail.com>
2024-01-09 17:11:12 +0300

    birim kısmına ait olan daire şube ve daire bilgi kısmı yazıldı

---
+++ JSGABackend.Entity/Concrete/Other/BirimKismi/Sube.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Entity.Concrete.Base;
+
+namespace JSGABackend.Entity.Concrete.Other.BirimKismi
+{
+	public class Sube: BaseEntity
+	{
+		public required Daire Daire { get; set; }
+		public required SubeBilgi SubeBilgi { get; set; }
+	}
+}
---
+++ JSGABackend.Entity/Concrete/Other/BirimKismi/SubeBilgi.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Entity.Concrete.Base;
+
+namespace JSGABackend.Entity.Concrete.Other.BirimKismi
+{
+	public class SubeBilgi : BaseEntity
+	{
+		public required string Ad { get; set; }
+		public required string Kisaltma { get; set; }
+	}
+}
