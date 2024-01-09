db373ecd971a6a48b114e41608b52511fd702703
Bekir PİRALP <bpir33@gmail.com>
2024-01-09 13:52:15 +0300

    Şahis tablosu eklendi

---
+++ JSGABackend.Entity/Concrete/Other/SahisKismi/Sahis.cs
@@ -0,0 +1,14 @@
+﻿using JSGABackend.Entity.Concrete.Base;
+
+namespace JSGABackend.Entity.Concrete.Other.SahisKismi
+{
+	public class Sahis : BaseEntity
+	{
+		public required string TC { get; set; }
+		public required string Pbik { get; set; }
+		public required string Ad { get; set; }
+		public required string Soyad { get; set; }
+		public byte[]? Foto { get; set; }
+
+	}
+}
