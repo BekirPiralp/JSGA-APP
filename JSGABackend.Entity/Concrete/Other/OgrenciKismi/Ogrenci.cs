9543428a9c3eae09b9361fe3d0c2faec971b91c8
Bekir PİRALP <bpir33@gmail.com>
2024-01-10 11:51:49 +0300

    Öğrenci kısmı eklendi diğer özellikler eklenecek.

---
+++ JSGABackend.Entity/Concrete/Other/OgrenciKismi/Ogrenci.cs
@@ -0,0 +1,13 @@
+﻿using JSGABackend.Entity.Concrete.Other.SahisKismi;
+
+namespace JSGABackend.Entity.Concrete.Other.OgrenciKismi
+{
+	public class Ogrenci :SahisBulunanBase
+	{
+		public required string OgrenciNo { get; set; }
+		public bool KitaKaynaklimi { get; set; } = false;
+		public float DisiplinPuani { get; set; } = 100;
+		public bool MezuniyetDurumu { get; set; } = false;
+		public float MezuniyetPuani { get; set; } = 0;
+	}
+}
