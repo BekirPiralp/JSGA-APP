ba494130c8a0d42f7afccd9caf0fe3254b742e3a
Bekir PİRALP <bpir33@gmail.com>
2024-01-12 12:13:57 +0300

    Derslik kısmı yazıldı: ders ve özel derslik kısmı yazıldı.

---
+++ JSGABackend.Entity/Concrete/Other/DerslikKismi/Derslik.cs
@@ -0,0 +1,11 @@
+﻿using JSGABackend.Entity.Concrete.Base;
+
+namespace JSGABackend.Entity.Concrete.Other.DerslikKismi
+{
+	public class Derslik : BaseEntity
+	{
+		public required string Adi { get; set; }
+		public required string Aciklama { get; set; }
+		public required int Kapasite { get; set; }
+	}
+}
---
+++ JSGABackend.Entity/Concrete/Other/DerslikKismi/OzelDerslik.cs
@@ -0,0 +1,11 @@
+﻿using JSGABackend.Entity.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.DersKismi;
+
+namespace JSGABackend.Entity.Concrete.Other.DerslikKismi
+{
+	public class OzelDerslik : BaseEntity
+	{
+		public required Ders Ders { get; set; }
+		public required Derslik Derslik { get; set; }
+	}
+}
