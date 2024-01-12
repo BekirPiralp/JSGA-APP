6c41e732f05b3a453b15f9683616525c79ff9b17
Bekir PİRALP <bpir33@gmail.com>
2024-01-12 12:34:32 +0300

    Ders Alma ve Verme kodlanıyor

---
+++ JSGABackend.Entity/Concrete/Other/DersAlmaVerme/GunlukIslemSaati.cs
@@ -0,0 +1,26 @@
+﻿using JSGABackend.Entity.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.OkulKismi;
+
+namespace JSGABackend.Entity.Concrete.Other.DersAlmaVerme
+{
+	public class GunlukIslemSaati : BaseEntity
+	{ 
+		public required Sinif Sinif { get; set; }
+		public required string SaatAdi { get; set; }
+		public required int SuresiDakika { get; set; }
+		public required int BaslangicSaat { get; set; }
+		public required int BaslangicDakika { get; set; }
+		public required int HaftadakiGun { get; set; } // pzt = 1 vs....
+	}
+
+	public enum HaftadakiGun
+	{
+		Pazartesi = 1,
+		Sali = 2,
+		Carsamba = 3,
+		Persembe = 4,
+		Cuma = 5,
+		Cumartesi = 6,
+		Pazar = 7
+	}
+}
