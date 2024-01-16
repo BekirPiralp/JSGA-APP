dae0d8eafc36ec8c5c4dcb57c1f74c68a2b5d042
Bekir PİRALP <bpir33@gmail.com>
2024-01-16 14:04:34 +0300

    Bağlamlar Contexts kısmı eklendi

---
+++ Contexts/EntityFrameworkContextMsSql.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contexts
+{
+	public class EntityFrameworkContextMsSql : DbContext
+	{
+		public EntityFrameworkContextMsSql() : base()
+		{
+
+		}
+
+		public EntityFrameworkContextMsSql(DbContextOptions<EntityFrameworkContextMsSql> options) : base(options)
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
