b0760a62cc20a89962b605e8b303d05e83acb72f
Bekir PİRALP <bpir33@gmail.com>
2024-01-16 13:51:53 +0300

    Data Access layer ın base arayüzü oluşturuldu. Response oluşturuldu

---
+++ DataAccess/Abstract/Base/IEntityBaseDataAccess.cs
@@ -0,0 +1,48 @@
+﻿using JSGABackend.Entity.Abstract;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract.Base
+{
+	public interface IEntityBaseDataAccess<TEntity> where TEntity : class, IEntity, new()
+	{
+		/**
+		 * Getirme
+		 * Ekleme
+		 * Silme
+		 * Güncelleme
+		 */
+
+		#region Getirme
+		Task<List<TEntity>> GetByFilterAsync (Expression<Func<TEntity, bool>>? filter = null);
+		Task<int> GetTotalCountByFilterAsync(Expression<Func<TEntity, bool>>? filter = null);
+
+		Task<List<TEntity>> GetPaginationByFilterAsync(
+			int pageItemsCount, 
+			int pageIndex,
+			bool desc = false, 
+			Expression<Func<TEntity, bool>>? filter = null);
+		Task<List<TEntity>> GetPaginationByFilterAsync<TKey>( 
+			int pageItemsCount,
+			int pageIndex,
+			bool desc = false,
+			Expression<Func<TEntity,bool>>? filter = null,
+			Expression<Func<TEntity,TKey>>? orderFilter = null);
+
+		#endregion
+
+		#region Kaydetme
+		Task<TEntity> CreateAsync(TEntity entity,DateTime createTime);
+
+		#endregion
+
+		#region Günclleme
+		Task<TEntity> UpdateAsync(TEntity entity,DateTime updateTime);
+
+		#endregion
+
+		#region Silme
+		Task<TEntity> DeleteAsync(TEntity entity,DateTime deletedTime);
+
+		#endregion
+	}
+}
---
+++ Result/Response.cs
@@ -0,0 +1,50 @@
+﻿namespace Responses
+{
+	public class Response<T>
+	{
+		private bool _status = false;
+		private bool _messageStatus = false;
+		private string? _message;
+		private T? _result = default(T); // varsayılanı null ise null olur
+		private Response() { }
+		public Response(T t,bool status) {
+			_status = status;
+			_result = t;
+		}
+
+		public Response(bool status,string? message) {
+			_status = status;
+			_message = message;
+		}
+
+		public Response(string? message) {
+			this.message = message;
+		}
+		public bool Status { 
+			get { return _status; } 
+		}
+		public string? Message
+		{
+			get {				
+				return _messageStatus ? _message : null ; 
+			}
+		}
+
+		private string? message
+		{
+			set
+			{
+				if (value == null || value.Trim().Length<0)
+				{
+					_message = null;
+					_messageStatus = false;
+				}
+				else
+				{
+					_message = value;
+					_messageStatus = true;
+				}
+			}
+		}
+	}
+}
