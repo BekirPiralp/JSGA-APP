81052e511e3ba4e7585fb2091f77a1283b883c20
Bekir PİRALP <bpir33@gmail.com>
2024-01-24 17:11:44 +0300

    Data Access - Abstract - Other - Ders Kismi yazıldı.

---
+++ DataAccess/Abstract/Other/DersKismi/IDersIcerikDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.DataAccess.Abstract.Base.EntityFramework;
+using JSGABackend.Entity.Concrete.Other.DersKismi;
+
+namespace JSGABackend.DataAccess.Abstract.Other.DersKismi
+{
+	public interface IDersIcerikDataAccess : IEntityBaseForEntityFrameworkDataAccess<DersIcerik>
+	{
+	}
+}
