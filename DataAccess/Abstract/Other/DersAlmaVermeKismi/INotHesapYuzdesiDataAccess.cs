4187fb0a679b0da1fbd6ffb933a25ec99cdfbeb6
Bekir PİRALP <bpir33@gmail.com>
2024-01-24 16:55:29 +0300

    Data Access - Abstract - Other -Ders Alma Verme Data Access kısmı yazıldı.

---
+++ DataAccess/Abstract/Other/DersAlmaVermeKismi/INotHesapYuzdesiDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.DataAccess.Abstract.Base.EntityFramework;
+using JSGABackend.Entity.Concrete.Other.DersAlmaVermeKismi;
+
+namespace JSGABackend.DataAccess.Abstract.Other.DersAlmaVermeKismi
+{
+	public interface INotHesapYuzdesiDataAccess : IEntityBaseForEntityFrameworkDataAccess<NotHesapYuzdesi>
+	{
+	}
+}
--- JSGABackend.Entity/Concrete/Other/DersAlmaVermeKismi/NotHesapYuzdesi.cs
+++ JSGABackend.Entity/Concrete/Other/DersAlmaVermeKismi/NotHesapYuzdesi.cs
@@ -2,7 +2,7 @@
 
 namespace JSGABackend.Entity.Concrete.Other.DersAlmaVermeKismi
 {
-    public class NotHesapYüzdesi : BaseEntity
+    public class NotHesapYuzdesi : BaseEntity
     {
         public required float VizeYuzde { get; set; }
         public required float FinalYüzde { get; set; }
