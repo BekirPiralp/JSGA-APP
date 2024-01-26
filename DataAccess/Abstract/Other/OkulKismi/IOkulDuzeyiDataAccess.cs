44717636535f34b63d7da946041567cc1d79ccab
Bekir PİRALP <bpir33@gmail.com>
2024-01-26 11:49:42 +0300

    Data Access Layer - Abstract - Other Okul Kısmı yazımı tamamlandı.

---
+++ DataAccess/Abstract/Other/OkulKismi/IOkulDuzeyiDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.DataAccess.Abstract.Base.EntityFramework;
+using JSGABackend.Entity.Concrete.Other.OkulKismi;
+
+namespace JSGABackend.DataAccess.Abstract.Other.OkulKismi
+{
+	public interface IOkulDuzeyiDataAccess : IEntityBaseForEntityFrameworkDataAccess<OkulDuzeyi>
+	{
+	}
+}
