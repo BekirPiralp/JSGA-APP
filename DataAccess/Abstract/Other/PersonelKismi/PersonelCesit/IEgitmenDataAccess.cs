01c65043bfd46cf57c8a107e3e12e19d2fb65994
Bekir PİRALP <bpir33@gmail.com>
2024-01-26 12:07:58 +0300

    Data Access Layer - Abstract - Other - Personel Kısmı - Personel Çeşit ve Personel Unvan Kısımları yazılmıştır.

---
+++ DataAccess/Abstract/Other/PersonelKismi/PersonelCesit/IEgitmenDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.DataAccess.Abstract.Base.EntityFramework;
+using JSGABackend.Entity.Concrete.Other.PersonelKismi.PersonelCesit;
+
+namespace JSGABackend.DataAccess.Abstract.Other.PersonelKismi.PersonelCesit
+{
+	public interface IEgitmenDataAccess : IEntityBaseForEntityFrameworkDataAccess<Egitmen>
+	{
+	}
+}
---
+++ DataAccess/Abstract/Other/PersonelKismi/PersonelUnvan/IUnvanDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.DataAccess.Abstract.Base.EntityFramework;
+using JSGABackend.Entity.Concrete.Other.PersonelKismi.PersonelUnvan;
+
+namespace JSGABackend.DataAccess.Abstract.Other.PersonelKismi.PersonelUnvan
+{
+	public interface IUnvanDataAccess : IEntityBaseForEntityFrameworkDataAccess<Unvan>
+	{
+	}
+}
