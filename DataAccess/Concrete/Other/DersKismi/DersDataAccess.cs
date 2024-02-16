0048366b9ac0fe5b199b5128211300222909978c
Bekir PİRALP <bpir33@gmail.com>
2024-02-16 09:43:39 +0300

    Veri Erişim Katmanında bulunan somut nesnelerden diğerlerine ait olan ders kısmı yazımı tamamlanmıştır.

---
+++ DataAccess/Concrete/Other/DersKismi/DersDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.DersKismi;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.DersKismi;
+
+namespace JSGABackend.DataAccess.Concrete.Other.DersKismi
+{
+	public class DersDataAccess : 
+		EntityBaseForEntityFrameworkDataAccess<Ders,EntityFrameworkContextMsSql>,
+		IDersDataAccess
+	{
+	}
+}
