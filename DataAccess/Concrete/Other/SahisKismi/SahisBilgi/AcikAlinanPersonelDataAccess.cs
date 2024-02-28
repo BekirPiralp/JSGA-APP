d545dfbb8f5ca1591e2b43b8e6b9cbb848f5c50f
Bekir PİRALP <bpir33@gmail.com>
2024-02-28 15:41:50 +0300

    Veri Erişim Katmanı - somut -diğer - Sahis kısmı kodlandı

---
+++ DataAccess/Concrete/Other/SahisKismi/SahisBilgi/AcikAlinanPersonelDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.SahisKismi.SahisBilgi;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.SahisKismi.SahisBilgi;
+
+namespace JSGABackend.DataAccess.Concrete.Other.SahisKismi.SahisBilgi
+{
+	public class AcikAlinanPersonelDataAccess : EntityBaseForEntityFrameworkDataAccess<AcikAlinanPersonel,
+		EntityFrameworkContextMsSql>,
+		IAcikAlinanPersonelDataAccess
+	{
+	}
+}
---
+++ DataAccess/Concrete/Other/SahisKismi/SahisDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.SahisKismi;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.SahisKismi;
+
+namespace JSGABackend.DataAccess.Concrete.Other.SahisKismi
+{
+	public class SahisDataAccess : EntityBaseForEntityFrameworkDataAccess<Sahis,EntityFrameworkContextMsSql>,
+		ISahisDataAccess
+	{
+	}
+}
