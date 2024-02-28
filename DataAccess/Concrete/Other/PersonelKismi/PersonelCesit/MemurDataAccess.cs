9094ce13f38eb96316deeef35900f19cf328acfe
Bekir PİRALP <bpir33@gmail.com>
2024-02-28 15:19:01 +0300

    VeriErişim katmanında mevcut olması gereken somut - diğer - personel kısmı yazıldı

---
+++ DataAccess/Concrete/Other/PersonelKismi/PersonelCesit/MemurDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.PersonelKismi.PersonelCesit;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.PersonelKismi.PersonelCesit;
+
+namespace JSGABackend.DataAccess.Concrete.Other.PersonelKismi.PersonelCesit
+{
+	public class MemurDataAccess : EntityBaseForEntityFrameworkDataAccess<Memur,EntityFrameworkContextMsSql>,
+		IMemurDataAccess
+	{
+	}
+}
---
+++ DataAccess/Concrete/Other/PersonelKismi/PersonelUnvan/RutbeDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.PersonelKismi.PersonelUnvan;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.PersonelKismi.PersonelUnvan;
+
+namespace JSGABackend.DataAccess.Concrete.Other.PersonelKismi.PersonelUnvan
+{
+	public class RutbeDataAccess : EntityBaseForEntityFrameworkDataAccess<Rutbe,EntityFrameworkContextMsSql>,
+		IRutbeDataAccess
+	{
+	}
+}
