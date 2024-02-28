028069dad91b6c24441c4567a790682968a4b796
Bekir PİRALP <bpir33@gmail.com>
2024-02-28 15:05:26 +0300

    Veri erişim katmanı somut kısmından diğerlerinden okul kısmı yazılmıştır.

---
+++ DataAccess/Concrete/Other/OkulKismi/OkulDuzeyiDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.OkulKismi;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.OkulKismi;
+
+namespace JSGABackend.DataAccess.Concrete.Other.OkulKismi
+{
+	public class OkulDuzeyiDataAccess : EntityBaseForEntityFrameworkDataAccess<OkulDuzeyi,EntityFrameworkContextMsSql>,
+		IOkulDuzeyiDataAccess
+	{
+	}
+}
---
+++ DataAccess/Concrete/Other/OkulKismi/SinifDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using JSGABackend.Contexts;
+using JSGABackend.DataAccess.Abstract.Other.OkulKismi;
+using JSGABackend.DataAccess.Concrete.Base;
+using JSGABackend.Entity.Concrete.Other.OkulKismi;
+
+namespace JSGABackend.DataAccess.Concrete.Other.OkulKismi
+{
+	public class SinifDataAccess : EntityBaseForEntityFrameworkDataAccess<Sinif,EntityFrameworkContextMsSql>,
+		ISinifDataAccess
+	{
+	}
+}
