fa252d75fc7d4098bab18e426cab0ccd496649e8
Bekir PİRALP <bpir33@gmail.com>
2024-04-15 11:40:29 +0300

    klasör isim düzeltmesi yapıldı

---
+++ JSGABackend.Business/Abstract/Base/IBaseForBusiness.cs
@@ -0,0 +1,64 @@
+﻿using JSGABackend.Entity.Abstract;
+using JSGABackend.Responses;
+using System.Reflection;
+
+namespace JSGABackend.Business.Abstract.Base
+{
+	public interface IBaseForBusiness<TEntity>
+		where TEntity : class,IEntity
+	{
+		/**
+		 * Getirme
+		 * Ekleme
+		 * Güncelleme
+		 * Silme
+		 */
+
+		#region Getirme
+
+		public Task<Response<TEntity>> GetByID(int Id);
+
+		public Task<Response<List<TEntity>>> GetAll();
+
+		public Task<Response<int>>GetCount();
+
+		public Task<Response<List<TEntity>>> GetPage(int pageIndex, int pageItemSize, string? orderField, bool desc);
+
+		public Task<Response<List<TEntity>>> GetSearch(string? searchKey);
+
+		public Task<Response<List<TEntity>>> GetSearchAndPage(int pageIndex, int pageItemSize, string? orderFileds,
+			string? searchString, bool desc,params string[]? removeColumns);
+
+		public Task<Response<List<TEntity>>> GetAllRemoved();
+
+		public Task<Response<int>> GetRemovedCount();
+
+		public Task<Response<int>> GetAllTotalCount();
+
+		#endregion
+
+		#region Ekleme
+
+		public Task<Response<TEntity>> Add(TEntity entity);
+
+		public Task<Response<bool>> Add(List<TEntity> entities);
+
+		#endregion
+
+		#region Silme
+
+		public Task<Response<bool>> Remove(TEntity entity);
+
+		public Task<Response<bool>> Remove(int Id);
+
+		#endregion
+
+		#region Güncelleme
+
+		public Task<Response<TEntity>> Update(TEntity entity);
+
+		public Task<Response<List<TEntity>>> Update(List<TEntity> entity);
+
+		#endregion
+	}
+}
