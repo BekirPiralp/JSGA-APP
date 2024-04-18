747adfad5a8a4dfe75ec9d7216f6d8f44c998b0e
Bekir PİRALP <bpir33@gmail.com>
2024-04-18 16:48:39 +0300

    İş katmanı interface'de akademik kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/AkademikKismi/IAkademikHaftaBusiness.cs
@@ -0,0 +1,9 @@
+using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.AkademikKisim;
+
+namespace JSGABackend.Business;
+
+public interface IAkademikHaftaBusiness : IBaseForBusiness<AkademikHafta>
+{
+
+}
---
+++ JSGABackend.Business/Abstract/Other/AkademikKismi/IAkademikHaftaOzelGunleriBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.AkademikKisim;
+
+namespace JSGABackend.Business;
+
+public interface IAkademikHaftaOzelGunleriBusiness : IBaseForBusiness<AkademikHaftaOzelGunleri>
+{
+
+}
