142e0e8529168130e769d30f87ff0d2b650c7501
Bekir PİRALP <bpir33@gmail.com>
2024-04-18 16:53:31 +0300

    İş katmanı interface'de Birim kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/BirimKismi/ISubeBilgiBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.BirimKismi;
+
+namespace JSGABackend.Business;
+
+public interface ISubeBilgiBusiness : IBaseForBusiness<SubeBilgi>
+{
+
+}
