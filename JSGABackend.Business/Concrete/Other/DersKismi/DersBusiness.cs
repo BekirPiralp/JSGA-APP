3773fbdb7f28fa1c17ac2a959f0c482444e07a32
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 14:09:34 +0300

    İş Katmanı uygulama alanınada ders kısmı yazıldı.

---
+++ JSGABackend.Business/Concrete/Other/DersKismi/DersBusiness.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Business.Concrete.Base;
+using JSGABackend.DataAccess.Abstract.Other.DersKismi;
+using JSGABackend.Entity.Concrete.Other.DersKismi;
+
+namespace JSGABackend.Business;
+
+public class DersBusiness : BaseForBusiness< IDersDataAccess, Ders>, IDersBusiness
+{
+
+}
