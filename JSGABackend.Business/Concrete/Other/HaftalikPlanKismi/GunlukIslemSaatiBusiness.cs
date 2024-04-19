cad40cf2e09ae04f6453fcd0630b1cf4e346edf8
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 14:27:36 +0300

    İş Katmanı uygulama alanında haftalık plan kısmı yazıldı.

---
+++ JSGABackend.Business/Concrete/Other/HaftalikPlanKismi/GunlukIslemSaatiBusiness.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Business.Concrete.Base;
+using JSGABackend.DataAccess.Abstract.Other.HaftalikPlanKismi;
+using JSGABackend.Entity.Concrete.Other.HaftalikPlanKismi;
+
+namespace JSGABackend.Business;
+
+public class GunlukIslemSaatiBusiness : BaseForBusiness< IGunlukIslemSaatiDataAccess, GunlukIslemSaati>,IGunlukIslemSaatiBusiness
+{
+
+}
