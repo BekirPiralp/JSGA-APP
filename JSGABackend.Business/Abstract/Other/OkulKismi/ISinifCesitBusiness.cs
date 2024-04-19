30d2ee15888016bbf5bcd055d7e05f48604b7aec
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:42:46 +0300

    İş katmanı arayüzünde Okul kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/OkulKismi/ISinifCesitBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.OkulKismi;
+
+namespace JSGABackend.Business;
+
+public interface ISinifCesitBusiness : IBaseForBusiness<SinifCesit>
+{
+
+}
