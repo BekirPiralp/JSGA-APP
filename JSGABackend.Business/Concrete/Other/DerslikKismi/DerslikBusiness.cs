bfe7bf047898f262d1b94eb4b60025eef344bce6
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 14:13:48 +0300

    İş katmanı uygulama alanında derslik kısmı yazıldı.

---
+++ JSGABackend.Business/Concrete/Other/DerslikKismi/DerslikBusiness.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Business.Concrete.Base;
+using JSGABackend.DataAccess.Abstract.Other.DerslikKismi;
+using JSGABackend.Entity.Concrete.Other.DerslikKismi;
+
+namespace JSGABackend.Business;
+
+public class DerslikBusiness : BaseForBusiness< IDerslikDataAccess, Derslik>, IDerslikBusiness
+{
+
+}
