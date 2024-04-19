5a6d369e102414ae742e6c59f4a40f6229132034
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:27:31 +0300

    İş katmanı arayüzünde ders kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/DersKismi/IDersBransBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.DersKismi;
+
+namespace JSGABackend.Business;
+
+public interface IDersBransBusiness : IBaseForBusiness<DersBrans>
+{
+
+}
