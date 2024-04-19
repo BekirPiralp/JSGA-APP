b5a09213fc74f1ef51e88d9e84a409258f4c3535
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 11:29:45 +0300

    İş katmanı somut kısmında Akademik kısmı yazıldı.

---
+++ JSGABackend.Business/Concrete/Other/AkademikKismi/AkademikEgitimBusiness.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Business.Concrete.Base;
+using JSGABackend.DataAccess.Abstract.Other.AkademiKismi;
+using JSGABackend.Entity.Concrete.Other.AkademikKisim;
+
+namespace JSGABackend.Business;
+
+public class AkademikEgitimBusiness : BaseForBusiness<IAkademikEgitimDataAccess,AkademikEgitim>, IAkademikEgitimBusiness
+{
+
+}
