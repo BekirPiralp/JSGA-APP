c497c8531f639de852eef7e430ddef33414ec67f
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 14:31:04 +0300

    İş katmanı uygulama alanında ogrenci kısmı yazıldı.

---
+++ JSGABackend.Business/Concrete/Other/OgrenciKismi/OgrenciBusiness.cs
@@ -0,0 +1,10 @@
+﻿using JSGABackend.Business.Concrete.Base;
+using JSGABackend.DataAccess.Abstract.Other.OgrenciKismi;
+using JSGABackend.Entity.Concrete.Other.OgrenciKismi;
+
+namespace JSGABackend.Business;
+
+public class OgrenciBusiness : BaseForBusiness<IOgrenciDataAccess, Ogrenci>, IOgrenciBusiness
+{
+
+}
