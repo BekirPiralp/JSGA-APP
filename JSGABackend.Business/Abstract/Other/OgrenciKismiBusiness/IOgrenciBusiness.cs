8ecb75e731761e41ad90ef366ca6772eda11ea4b
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:38:34 +0300

    İş katmanı arayüzünde öğrenci kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/OgrenciKismiBusiness/IOgrenciBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.OgrenciKismi;
+
+namespace JSGABackend.Business;
+
+public interface IOgrenciBusiness : IBaseForBusiness<Ogrenci>
+{
+
+}
