5d84b7537c71111704eea2e076490a0d68e0af08
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:34:08 +0300

    İş katmanı arayüzünde güvenlik kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/GuvenlikKismi/IJetonSureBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.GuvenlikKismi;
+
+namespace JSGABackend.Business;
+
+public interface IJetonSureBusiness : IBaseForBusiness<JetonSure>
+{
+
+}
