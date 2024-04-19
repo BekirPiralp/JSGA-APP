ca748dd752c6659b0a717bea3de1c49a0c4dd1f4
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:24:29 +0300

    İşkatmanı arayüz kısmında dersalma ve verme kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/DersAlmaVermeKismi/INotHesapYuzdesiBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.DersAlmaVermeKismi;
+
+namespace JSGABackend.Business;
+
+public interface INotHesapYuzdesiBusiness : IBaseForBusiness<NotHesapYuzdesi>
+{
+
+}
