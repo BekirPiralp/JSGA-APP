55c2f4b6bdd1ed8ca55567499eef6a4b479981a1
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:56:44 +0300

    İş katmanı arayüzünde Şahıs kısmı yazıldı

---
+++ JSGABackend.Business/Abstract/Other/SahisKismi/ISahisBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.SahisKismi;
+
+namespace JSGABackend.Business;
+
+public interface ISahisBusiness : IBaseForBusiness<Sahis>
+{
+
+}
---
+++ JSGABackend.Business/Abstract/Other/SahisKismi/SahisBilgi/ITelefonBilgisiBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.SahisKismi.SahisBilgi;
+
+namespace JSGABackend.Business;
+
+public interface ITelefonBilgisiBusiness : IBaseForBusiness<TelefonBilgisi>
+{
+
+}
