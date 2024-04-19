61daf34acbaf1253d5e7998a0d3a26a240de23bd
Bekir PİRALP <bpir33@gmail.com>
2024-04-19 09:37:05 +0300

    İş katmanı Haftalık plan kısmı yazıldı.

---
+++ JSGABackend.Business/Abstract/Other/HaftalikPlanKismi/IOgrenciDersHariciSaatBusiness.cs
@@ -0,0 +1,9 @@
+﻿using JSGABackend.Business.Abstract.Base;
+using JSGABackend.Entity.Concrete.Other.HaftalikPlanKismi;
+
+namespace JSGABackend.Business;
+
+public interface IOgrenciDersHariciSaatBusiness : IBaseForBusiness<OgrenciDersHariciSaat>
+{
+
+}
