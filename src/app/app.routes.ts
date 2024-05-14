edcaf190be0711f09f750a6d468b1ca7d0c3b5e7
Bekir PİRALP <bpir33@gmail.com>
2024-05-14 16:12:44 +0300

    Ana sayfanın büyük bölümü oluşturuldu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from '../components/Login/Login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Home', component: AppComponent},
+    {path:'#', component: AppComponent},
+    {path:'Login',component:LoginComponent},
+    {path:'login',component:LoginComponent},
+    {path:'',redirectTo:'/Home',pathMatch:'full'}
+];
