f64c239602cfba62e7c6344fecb3c19e66d6ef2d
Bekir PİRALP <bpir33@gmail.com>
2024-06-12 14:55:25 +0300

    transkript kısmı ayarlandı

--- src/components/Ogrenci/Other/Transkript/transkript.component.ts
+++ src/components/Ogrenci/Other/Transkript/transkript.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'Transkript',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transkript.component.html',
   styleUrl: './transkript.component.css',
 })
 export class TranskriptComponent {
 
+  private timeOutId:number=0;
+  mouseover(event:MouseEvent,id:string):void{
+    let element = document.getElementById(id);
+    let bilgiBox = element?.getElementsByClassName('bilgiBox')[0];
+
+    this.timeOutId = window.setTimeout(()=>{
+      if(bilgiBox !== undefined)
+        (bilgiBox! as HTMLElement).style.display='flex';
+    },1000);
+  }
+
+  mouseout(event:MouseEvent,id:string):void{
+    clearTimeout(this.timeOutId);
+
+    let element = document.getElementById(id);
+    let bilgiBox = element?.getElementsByClassName('bilgiBox')[0];
+    if(bilgiBox !== undefined)
+      (bilgiBox as HTMLElement).style.display='none';
+  }
 }
